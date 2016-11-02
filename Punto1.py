967591a745568a7780a58efe2bec13c86a7cfd90
Slackware Live User <live@darkstar.example.net>
2016-11-01 18:48:10 -0700

    parcial

--- Punto1.py
+++ Punto1.py
@@ -1,22 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import cmath as cm
-
-#eps1=1
-#mu1=1
-#eps2=2
-#mu2=1
+from matplotlib import rc
 
 eps0=1. 
 mu0=1.
 
-E2p=1.
-E2m=1.
-
 d=50.
-
 c=1.
+
 wp=1.
+w=np.linspace(wp/10,2*wp, num = 1000)
 
 def Interfaz(eps1, mu1, eps2, mu2):
     Y1=cm.sqrt(eps1/mu1)
@@ -27,33 +21,15 @@ def Interfaz(eps1, mu1, eps2, mu2):
 
 def Propagacion(d, w):
     k=(1/c)*cm.sqrt((w*w)-(wp*wp))
-    d=  d%(2*np.pi)
-    D = np.matrix([ [cm.exp(-1j*k*d),0],[0, cm.exp(1j*k*d)] ]) 
+    phi = (k*d)# %(2*np.pi)
+    print( phi)
+    D = np.matrix([ [cm.exp(-1j*phi),0],[0, cm.exp(1j*phi)] ]) 
     return D
 
 def eps(w):
 	eps= eps0 * (1.-(wp*wp)/(w*w))
 	return eps
-	
 
-E2 = np.matrix([ [E2p],[E2m] ])                  # Creates Electric Field.
-
-w=np.linspace(wp/10,2*wp, num = 1000)
-x=np.linspace(-d,2*d,num=100)
-
-eps_plasma= eps(w)
-#plt.plot(w, eps_plasma, 'o')
-#plt.show()
-
-#I=Interfaz(eps0,mu0,eps_plasma,mu0)
-#print I
-E1pNorm=[] #Norma E1+
-E1pPhase=[] #Fase E1+
-E1mNorm=[] #Norma E1-
-E1mPhase=[] #Fase E1-
-
-#E1=[]
-r=[]
 rNorm=[] #Norma r
 rPhase=[] #Fase r
 tNorm=[] #Norma t
@@ -68,6 +44,7 @@ R=[]
 T=[]
 Rp=[]
 Tp=[]
+
 RT=[]
 for f in w:
 	e=eps(f)
@@ -90,9 +67,6 @@ for f in w:
 	R.append(rr)
 	
 	#RT
-	#print rr
-	#print tt
-	#print rr+tt
 	RT.append(rr+tt)
 	
 	#rp
@@ -110,33 +84,20 @@ for f in w:
 	Tp.append(np.abs(tp)*np.abs(tp))
 	
 	
-	
-		
-	#Campos#
-	#E1 =A*E2
-	#E1p = np.array(E1[0])[0].tolist()
-	#E1m = np.array(E1[1])[0].tolist()
-	#print E1p[0]
-	#print E1m[0]
-	#print E1m[0]/E1p[0]
-	#E1pNorm.append(cm.polar(E1p[0])[0]) #Norma E1+
-	#E1pPhase.append(cm.polar(E1p[0])[1]) #Fase E1+
-	#E1mNorm.append(cm.polar(E1m[0])[0]) #Norma E1-
-	#E1mPhase.append(cm.polar(E1m[0])[1]) #Fase E1-
-	#print cm.polar(E_f[0])
-
 #Grafica de t	
 plt.plot(w, tNorm, label='t_N')
 plt.legend()
 plt.xlabel('w')
 plt.ylabel('Norma')
-plt.title('Norma de t')    
+plt.title('Norma de t')
+plt.savefig('t_Norm.pdf')
+
 #plt.show()
 plt.plot(w, tPhase, label='t_phase')
 plt.legend()
 plt.xlabel('w')
 plt.ylabel('Fase')
-plt.title('Fase de t')
+plt.title('t_Phase.pdf')
 #plt.show()
 
 #Grafica de r
@@ -224,18 +185,4 @@ plt.show()
 
 plt.plot(w,np.log(RT))
 plt.show()
-#print RT
-#print E1
-#print E1pn
-#plt.plot(w, E1pNorm)
-#plt.plot(w, E1pp)
-#plt.plot(w, E1mn)
-#plt.plot(w, E1mp)
-#plt.show()
-#plt.plot(x1, y, 'o')
-#[<matplotlib.lines.Line2D object at 0x...>]
-#>>> plt.plot(x2, y + 0.5, 'o')
-#[<matplotlib.lines.Line2D object at 0x...>]
-#>>> plt.ylim([-0.5, 1])
-#(-0.5, 1)
-#plt.show()
+
