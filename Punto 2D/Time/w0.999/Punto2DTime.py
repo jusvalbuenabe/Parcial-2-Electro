116b597d8e223294b40e6fb6f68f0df72e60270a
Juan Sebastián Valbuena B <jusvalbuenabe@unal.edu.co>
2016-11-04 13:48:18 -0500

    todo

--- Punto 2D/Time/w0.999/Punto2DTime.py
+++ Punto 2D/Time/w0.999/Punto2DTime.py
@@ -31,7 +31,7 @@ def Interfaz(eps1, mu1, eps2, mu2):
     return T 
 
 def Propagacion(d, w, eps):
-    k=(1./c)*(w*w)*cm.sqrt(eps)
+    k=(1./c)*(w)*cm.sqrt(eps)
     D= np.matrix([ [cm.exp(-1j*k*d),0],[0, cm.exp(1j*k*d)] ]) 
     return D
 
@@ -56,7 +56,7 @@ nf=str(a)
 for ii in t:
     #print ii
     #Fase= np.matrix([ [cm.exp(-1j*w*T/N),0],[0, cm.exp(-1j*w*T/N)] ]) 
-    E2=np.matrix([ [E2p*cm.exp(1j*w*ii*T)],[E2m*cm.exp(1j*w*ii*T)] ])
+    E2=np.matrix([ [E2p*cm.exp(1j*w*ii*T)],[E2m*cm.exp(-1j*w*ii*T)] ])
    
     #Fase= np.matrix([ [cm.exp(-1j*w*T/N),0],[0, cm.exp(-1j*w*T/N)] ]) 
     # E2=Fase*E2
